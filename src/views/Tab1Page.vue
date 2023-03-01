10cbf20375036cefeccac0c397993102eba8edaf
Amit Pandey <akpa1994@gmail.com>
2023-03-01 19:25:29 +0530

    Update Tab1Page.vue

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -12,12 +12,26 @@
         </ion-toolbar>
       </ion-header>
 
-      <ExploreContainer name="Tab 1 page" />
+  
+      <ion-item>
+        <ion-checkbox slot="start" disabled="true"></ion-checkbox>
+        <ion-label>I agree to the terms and conditions</ion-label>
+      </ion-item>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonCheckbox,
+  IonItem,
+  IonLabel,
+} from "@ionic/vue";
+import { add } from "ionicons/icons";
+import ExploreContainer from "@/components/ExploreContainer.vue";
 </script>
